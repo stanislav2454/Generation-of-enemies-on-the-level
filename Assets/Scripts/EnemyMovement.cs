fe8bc2df62f70af067ed5dff77a54f7ff53814fc
stanislav2454 <StanislavViktorovich15@yandex.ru>
2025-07-30 13:25:32 +0100

    рабочая версия

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    private Vector3 direction;
+
+    public void SetDirection(Vector3 newDirection)
+    {
+        direction = newDirection.normalized;
+    }
+
+    public void SetRandomDirection()
+    {
+        float randomX = Random.Range(-1f, 1f);
+        float randomZ = Random.Range(-1f, 1f);
+
+        direction = new Vector3(randomX, 0f, randomZ).normalized;
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * _speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints; 
+    [SerializeField] private GameObject enemyPrefab; 
+    [SerializeField] private float spawnInterval = 2f; 
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (enabled) 
+        {
+            yield return new WaitForSeconds(spawnInterval); 
+
+            Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+            GameObject newEnemy = Instantiate(enemyPrefab, randomSpawnPoint.position, randomSpawnPoint.rotation);
+
+            EnemyMovement enemyMovement = newEnemy.GetComponent<EnemyMovement>();
+
+            if (enemyMovement != null)
+            {
+                // два варианта движения: в указанном или рандомном направлнии
+                enemyMovement.SetRandomDirection();
+                //enemyMovement.SetDirection(randomSpawnPoint.forward);
+            }
+        }
+    }
+}
